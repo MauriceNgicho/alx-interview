757f71758bf6604f42f2275608abbc32fd936500
Maurice Ngicho <mrcngicho@gmail.com>
2025-04-19 20:00:56 +0300

    Pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+def pascal_triangle(n):
+    """
+    Returns a list of lists representing Pascal's triangle of n
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # First row
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        # Generate next row by adding adjacent elements in the previous row
+        row = [1]  # First element is always 1
+        for j in range(1, len(prev_row)):
+            row.append(prev_row[j - 1] + prev_row[j])
+        row.append(1)  # Last element is always 1
+        triangle.append(row)
+
+    return triangle
